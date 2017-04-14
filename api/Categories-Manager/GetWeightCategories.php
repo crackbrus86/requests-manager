5e9c1633eff41226c51a94d68edad8e3b8476383
crackbrus86 <crackbrus86@gmail.com>
2017-04-15 00:13:47 +0300

    Created form for requests

--- api/Categories-Manager/GetWeightCategories.php
+++ api/Categories-Manager/GetWeightCategories.php
@@ -2,7 +2,6 @@
 include_once("../wpdb-connect.php");
 $tb_name = $wpdb->get_blog_prefix() . 'rm_category_weight';
 $tb_name_age = $wpdb->get_blog_prefix() . 'rm_category_age';
-// $weightCategories = $wpdb->get_results( "SELECT * FROM $tb_name" );
 $weightCategories = $wpdb->get_results("SELECT $tb_name.id, $tb_name.title_w, $tb_name_age.title FROM $tb_name JOIN  $tb_name_age ON $tb_name.parent = $tb_name_age.id");
 $return = json_encode($weightCategories);
 print_r($return);
--- js/request-form.js
+++ js/request-form.js
@@ -1,10 +1,99 @@
 (function($, undefined) {
     $(document).ready(function() {
+
+        var dir = "../wp-content/plugins/requests-manager/api/";
+
+        var ageCategories = [],
+            weightCategories = [],
+            currentCompetition = [];
+        var typeOfCompetition;
+
+        $.ajax({
+            type: "POST",
+            url: dir + "Categories-Manager/GetWeightCategoriesStrict.php",
+            success: function(data) {
+                weightCategories = JSON.parse(data);
+            }
+        }).then(function() {
+            $.ajax({
+                type: "POST",
+                url: dir + "Categories-Manager/GetAgeCategories.php",
+                success: function(data) {
+                    ageCategories = JSON.parse(data);
+                }
+            }).then(function() {
+                appendOptions("#ageCategory", ageCategories);
+                appendOptions("#weightCategory", weightCategories.filter(function(item) {
+                    return item.parent == ageCategories[0].id;
+                }));
+            });
+        });
+
+        $.ajax({
+            type: "POST",
+            url: dir + "Games-Manager/GetActualGames.php",
+            success: function(data) {
+                currentCompetition = JSON.parse(data);
+            }
+        }).then(function() {
+            appendOptions("#currentCompetition", currentCompetition);
+            getExercises(getTypeOfCompetition("#currentCompetition", currentCompetition));
+        });
+
+
+
         $("#birthDate").datepicker({
             altFormat: "dd-mm-yy",
             changeYear: true,
             yearRange: "1910:2200",
             regional: ["uk"]
         });
-    })
+
+        $("#termOfPass").datepicker({
+            altFormat: "dd-mm-yy",
+            changeYear: true,
+            yearRange: "1990:2200",
+            regional: ["uk"]
+        });
+
+        $("#showNext").on("click", function() {
+            $(".anotherData").fadeIn(800);
+        });
+
+        $("#ageCategory").on("change", function(e) {
+            appendOptions("#weightCategory", weightCategories.filter(function(item) {
+                return item.parent == e.target.value;
+            }));
+        });
+
+        $("#currentCompetition").on("change", function(e) {
+            getExercises(getTypeOfCompetition("#currentCompetition", currentCompetition));
+        });
+    });
+
+    function appendOptions(select, data) {
+        var list = '';
+        data.forEach(function(item) {
+            var id = item.id;
+            var title = item.title || item.title_w || item.name;
+            list += '<option value="' + id + '">' + title + '</option>';
+        });
+        $(select).html(list);
+    }
+
+    function getTypeOfCompetition(select, listOfCompetitions) {
+        for (var i = 0; i < listOfCompetitions.length; i++) {
+            if (listOfCompetitions[i].id === $(select).val()) return listOfCompetitions[i].type;
+        }
+    }
+
+    function getExercises(type) {
+        if (type == 0) {
+            $("#squat").attr("disabled", false);
+            $("#deadLift").attr("disabled", false);
+        } else {
+            $("#squat").attr("disabled", true);
+            $("#deadLift").attr("disabled", true);
+        }
+    }
 })(jQuery)
