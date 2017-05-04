9e32a81025198509bfef92100c1aa82efbcef9c0
crackbrus86 <crackbrus86@gmail.com>
2017-05-05 01:00:14 +0300

    UploadFile: Created transliteration feature (for cyrillic texts)

--- api/UploadPhoto.php
+++ api/UploadPhoto.php
@@ -13,11 +13,14 @@ foreach($_FILES as $file) {
             exit;
         }
 		if (is_uploaded_file($file['tmp_name'])) {
-			if(!move_uploaded_file($file['tmp_name'], ABSPATH .'/wp-content/uploads/pictures/'.$file['name']))
+                $file_name = $file['name'];
+                $tmp_file_name = explode(".", $file_name);
+                $ext = $tmp_file_name[(count($tmp_file_name) - 1)]; 
+                $new_file_name =  transliteration(str_replace('.'.$ext, '', $file_name)).'.'.$ext;
+			if(!move_uploaded_file($file['tmp_name'], ABSPATH .'/wp-content/uploads/pictures/'.$new_file_name))
 				die("Error");
 			else 
-                $file_name = $file['name'];
-                $file_url = ABSPATH .'/wp-content/uploads/pictures/'.$file['name'];
+                $file_url = ABSPATH .'/wp-content/uploads/pictures/'.$new_file_name;
                 $file_type = $file['type'];
                 $attachment_id = upload_user_file( $file_name, $file_url, $file_type);
                 $nonce = wp_nonce_url( admin_url( 'tools.php?page=regenerate-thumbnails&goback=1&ids=' . $attachment_id ), 'regenerate-thumbnails' );
@@ -42,5 +45,21 @@ function upload_user_file( $file, $file_url, $file_type) {
                 return $attachment_id;
           }
       return false;
+}
+
+function transliteration($str){
+    $trans = array("а"=>"a","б"=>"b","в"=>"v","г"=>"g","д"=>"d","е"=>"e", 
+"ё"=>"yo","ж"=>"j","з"=>"z","и"=>"i","й"=>"i","к"=>"k","л"=>"l", 
+"м"=>"m","н"=>"n","о"=>"o","п"=>"p","р"=>"r","с"=>"s","т"=>"t", 
+"у"=>"y","ф"=>"f","х"=>"h","ц"=>"c","ч"=>"ch", "ш"=>"sh","щ"=>"shh",
+"ы"=>"i","э"=>"e","ю"=>"u","я"=>"ya","ї"=>"i","'"=>"","ь"=>"","Ь"=>"",
+"ъ"=>"","Ъ"=>"","і"=>"i","А"=>"A","Б"=>"B","В"=>"V","Г"=>"G","Д"=>"D",
+"Е"=>"E", "Ё"=>"Yo","Ж"=>"J","З"=>"Z","И"=>"I","Й"=>"I","К"=>"K", "Л"=>"L",
+"М"=>"M","Н"=>"N","О"=>"O","П"=>"P", "Р"=>"R","С"=>"S","Т"=>"T","У"=>"Y",
+"Ф"=>"F", "Х"=>"H","Ц"=>"C","Ч"=>"Ch","Ш"=>"Sh","Щ"=>"Sh", "Ы"=>"I","Э"=>"E",
+"Ю"=>"U","Я"=>"Ya","Ї"=>"I","І"=>"I");
+    $res=str_replace(" ","-",strtr(strtolower($str),$trans));
+    $res=preg_replace("|[^a-zA-Z0-9-]|","",$res);
+    return $res;
 }
 ?> 
