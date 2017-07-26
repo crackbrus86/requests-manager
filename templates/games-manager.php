aab552d3062ac041972d40071ed8fc0673e5e35a
qa <qa@coaliance.com>
2017-07-27 01:00:55 +0300

    Games react renovation: uncomment functional

--- templates/games-manager.php
+++ templates/games-manager.php
@@ -1,6 +1,6 @@
 <?php
-    // wp_register_script( 'games_manager_script', plugins_url( '../js/games-manager.js', __FILE__ ) );
-    // wp_enqueue_script(  'games_manager_script');
+    wp_register_script( 'games_manager_script', plugins_url( '../js/games-manager.js', __FILE__ ) );
+    wp_enqueue_script(  'games_manager_script');
     wp_register_script( 'games_manager_entry_script', plugins_url( '../js/games/games-bundle.js', __FILE__ ) );
     wp_enqueue_script(  'games_manager_entry_script');
 ?>      
