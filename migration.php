c7bb52958e100faea11ad51012b449e55a022256
Eugene Salyvon <crackbrus86@gmail.com>
2023-05-23 22:25:38 +0300

    Updated migration.php

--- migration.php
+++ migration.php
@@ -5,11 +5,16 @@ global $wpdb;
 // $result = $wpdb->get_results("ALTER TABLE $tb_users ADD certificate_adel BIGINT DEFAULT NULL");
 
 //01 Apr 2023
-$tb_others = $wpdb->get_blog_prefix() . 'rm_others';
-$result = $wpdb->get_results("ALTER TABLE $tb_others ADD president_name_latin VARCHAR(250) DEFAULT NULL");
-$result = $wpdb->get_results("ALTER TABLE $tb_others ADD date_of_birth DATE DEFAULT NULL");
-$result = $wpdb->get_results("ALTER TABLE $tb_others ADD foreign_pass_no_prefix VARCHAR(4) DEFAULT NULL");
-$result = $wpdb->get_results("ALTER TABLE $tb_others ADD foreign_pass_no VARCHAR(6) DEFAULT NULL");
+// $tb_others = $wpdb->get_blog_prefix() . 'rm_others';
+// $result = $wpdb->get_results("ALTER TABLE $tb_others ADD president_name_latin VARCHAR(250) DEFAULT NULL");
+// $result = $wpdb->get_results("ALTER TABLE $tb_others ADD date_of_birth DATE DEFAULT NULL");
+// $result = $wpdb->get_results("ALTER TABLE $tb_others ADD foreign_pass_no_prefix VARCHAR(4) DEFAULT NULL");
+// $result = $wpdb->get_results("ALTER TABLE $tb_others ADD foreign_pass_no VARCHAR(6) DEFAULT NULL");
+
+//23 May 2023
+// echo "Add ADEL Certificate to users table...";
+// $tb_users = $wpdb->get_blog_prefix().'rm_users';
+// $result = $wpdb->get_results("ALTER TABLE $tb_users ADD certificate_adel BIGINT DEFAULT NULL");
 
 echo "Done";
 ?>
